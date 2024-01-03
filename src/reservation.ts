5d60c5cd244487eeea73cf0862a6a3d0a271b08b
Michał Garapich <michal@garapich.pl>
2024-01-03 16:16:22 +0100

    feat(*): wait for gameserver to start

--- src/reservation.ts
+++ src/reservation.ts
@@ -1,6 +1,10 @@
 import { Client } from './client';
 import { Response } from './types/serveme-tf-responses';
 import { ReservationDetails } from './reservation-details';
+import { ReservationStatus } from './types/reservation-status';
+import { InvalidReservationStatusError } from './errors/invalid-reservation-state.error';
+import { secondsToMilliseconds } from 'date-fns';
+import { withTimeout } from './with-timeout';
 
 export class Reservation extends ReservationDetails {
   constructor(
@@ -28,5 +32,33 @@ export class Reservation extends ReservationDetails {
 
     this.setResponse(response.reservation);
     return this;
+  }
+
+  async waitForStarted(timeoutMs = secondsToMilliseconds(30)): Promise<this> {
+    let interval: ReturnType<typeof setInterval>;
+    return withTimeout(
+      new Promise<this>((resolve, reject) => {
+        interval = setInterval(async () => {
+          await this.refresh();
+
+          if (
+            [ReservationStatus.ending, ReservationStatus.ended].includes(
+              this.status,
+            )
+          ) {
+            return reject(new InvalidReservationStatusError(this.status));
+          }
+
+          if (
+            [ReservationStatus.ready, ReservationStatus.sdrReady].includes(
+              this.status,
+            )
+          ) {
+            return resolve(this);
+          }
+        }, secondsToMilliseconds(5));
+      }),
+      timeoutMs,
+    ).finally(() => clearInterval(interval));
   }
 }
